12ccdf6450243864d52245aa7d36ffa04638134a
Benjamin Haddad <haddad.b.pro@gmail.com>
2021-08-01 14:24:37 +0200

    light refactoring

--- Learning.React/Messenger/Front-end/src/components/Messenger/Messenger.tsx
+++ Learning.React/Messenger/Front-end/src/components/Messenger/Messenger.tsx
@@ -1,31 +1,52 @@
 import styles from './Messenger.module.css'
 import cn from 'classnames'
 
+
+type MessageDestination = 'EVERYBODY' | 'GROUP' | 'PRIVATE'
+type MessageFrom = 'USER' | 'OTHER_USER'
+
+interface MessageProps
+{
+    userName : string
+    content : string
+    destination : MessageDestination
+    from : MessageFrom
+    groupName? : string
+    destinationUserName? : string
+}
+
+const Message = ({userName, content, destination, from, groupName, destinationUserName} : MessageProps) =>
+{
+    const messageTitleFirstPart =  `${from === 'USER' ? 'You' : userName} sent a message to `
+    const messageTitleSecondPart = 
+        destination === 'EVERYBODY' ? 'everybody' : 
+        destination === 'GROUP' ? `the group ${groupName}` :
+        `the user ${destinationUserName}`
+    const messageTitle = `${messageTitleFirstPart}${messageTitleSecondPart}`
+
+    const messageClasses = cn(
+        styles.message,
+        destination === 'EVERYBODY' && styles.messageToEverybody,
+        destination === 'GROUP' && styles.groupMessage,
+        destination === 'PRIVATE' && styles.privateMessage,
+        from === 'USER' && styles.messageFromUser,
+        from === 'OTHER_USER' && styles.messageFromOtherUser)
+
+    return <>
+        <div className={styles.messageTitle}>{messageTitle}</div>
+        <div className={messageClasses}>{content}</div>
+    </>
+}
+
 const Messenger = () =>
 {
-    const messages = [
+    const messages : MessageProps[] = [
         { userName : 'Ben', content : 'lorem', destination : 'EVERYBODY', from : 'USER' },
         { userName : 'Jess', content : 'blabla', destination : 'GROUP', from : 'OTHER_USER', groupName : 'My great company' },
         { userName : 'David', content : 'lipsum', destination : 'PRIVATE', from : 'OTHER_USER', destinationUserName : 'Jess' }]
 
     return <div className={styles.container}>
-        {messages.map(m => 
-        <div className={styles.messageTitle}>{m.from === 'USER' ? 'You' : m.userName} sent a message to {
-                m.destination === 'EVERYBODY' ? 'everybody' : 
-                m.destination === 'GROUP' ? `the group ${m.groupName}` :
-                `the user ${m.destinationUserName}`
-                }
-            <div className={cn(
-                styles.message,
-                m.destination === 'EVERYBODY' && styles.messageToEverybody,
-                m.destination === 'GROUP' && styles.groupMessage,
-                m.destination === 'PRIVATE' && styles.privateMessage,
-                m.from === 'USER' && styles.messageFromUser,
-                m.from === 'OTHER_USER' && styles.messageFromOtherUSer
-            )}>
-                {m.content}
-            </div>
-        </div>)}
+        {messages.map((m, i) => <Message {...m} key={i} />)}
     </div>
 }
 
