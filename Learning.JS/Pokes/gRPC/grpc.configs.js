b4a74845d9aab882a2a7a114904d41ba6c1b3372
Benjamin Haddad <haddad.b.pro@gmail.com>
2021-08-13 13:55:05 +0200

    gRPC Server

---
+++ Learning.JS/Pokes/gRPC/grpc.configs.js
@@ -0,0 +1,12 @@
+const grpcOptions = {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+};
+
+module.exports = {
+    grpcOptions,
+    grpcServerUrl : "127.0.0.1:30043"
+};
