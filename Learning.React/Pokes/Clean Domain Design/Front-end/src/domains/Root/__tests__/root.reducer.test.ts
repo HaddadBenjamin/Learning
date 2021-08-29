ada46f6a02a420399b234fd9c077fe0a0a9edad3
Benjamin Haddad <haddad.b.pro@gmail.com>
2021-08-29 11:13:46 +0200

    root.reducer.test.ts

--- Learning.React/Pokes/Clean Domain Design/Front-end/src/domains/Root/__tests__/root.reducer.test.ts
+++ Learning.React/Pokes/Clean Domain Design/Front-end/src/domains/Root/__tests__/root.reducer.test.ts
@@ -1,15 +1,15 @@
 import {initialTodoState} from "../../Todo/todo.state";
 import rootReducer from "../root.reducer";
 import {createStore} from "redux";
+import store from "../root.store";
 
 describe("root.reducer", () =>
 {
     it("rootReducer.state should be equals to initialState", () => {
         // Given & When
-        const store = createStore(rootReducer)
-
         const todoState = store.getState().todos
 
+        // Then
         expect(todoState).toEqual(initialTodoState)
     })
 })
