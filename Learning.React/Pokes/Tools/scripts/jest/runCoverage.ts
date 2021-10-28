5f02d787baaf609669f85a4eaabb4d2b81707fda
Benjamin Haddad <haddad.b.pro@gmail.com>
2021-10-28 18:32:35 +0200

    wip

--- Learning.React/Pokes/Tools/scripts/jest/runCoverage.ts
+++ Learning.React/Pokes/Tools/scripts/jest/runCoverage.ts
@@ -9,11 +9,6 @@ if (parameters.length === 0)
   );
 else {
   try {
-    // const coverageThredshold =  '--coverageThreshold=\'{\"global\": { `\"branches`\": 100, \"functions\": 0, \"lines\": 0, \"statements\": 0 },
-    // \"src/domains/todos\": { \"branches\": 90, \"functions\": 90, \"lines\": 90, \"statements\": 90 },
-    // \"src/domains/root\": {\"branches\": 90, \"functions\": 90, \"lines\": 90, \"statements\": 90 },
-    // \"src/devTools\": {\"branches\": 90, \"functions\": 90, \"lines\": 90, \"statements\": 90 } }'
-    // const coverageThredshold = `--coverageThreshold='{\"global\":{\"statements\":\"80\"}}`
     const coverageBasePath = parameters[0];
     const otherParameters =
       parameters.length >= 1 ? parameters.splice(1).map(p => `--${p}`) : [];
