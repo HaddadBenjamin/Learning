d656297bffbcc1aa6a365ae32d6e653d06e2df7d
Benjamin Haddad <haddad.b.pro@gmail.com>
2021-11-02 11:29:42 +0100

    test an useState setter

--- Learning.React/Clean Domain Design/Front-end/src/domains/todo/components/AddTodo/__tests__/AddTodo.test.tsx
+++ Learning.React/Clean Domain Design/Front-end/src/domains/todo/components/AddTodo/__tests__/AddTodo.test.tsx
@@ -3,22 +3,24 @@ import AddTodo from "../AddTodo";
 import {useDispatch} from "react-redux";
 import {addTodoRequestAction} from "domains/todo/todo.action";
 import {todoTitleMock} from "domains/todo/todo.mock";
+import React from "react";
 
 jest.mock('react-redux')
 const mockUseDispatch = useDispatch as jest.MockedFunction<typeof useDispatch>
 const mockDispatch = jest.fn()
 
-describe("AddTodo", () =>
-{
-    beforeEach(() => {
-        (mockUseDispatch as jest.Mock).mockReturnValue(mockDispatch)
-    })
-
-    it("should match snapshot", () =>
-    {
+describe("AddTodo", () => {
+    const setState = jest.fn();
+    const useStateMock: any = (initState: any) => [initState, setState];
+    
+    afterEach(() => jest.clearAllMocks());
+    
+    beforeEach(() => (mockUseDispatch as jest.Mock).mockReturnValue(mockDispatch))
+    
+    it("should match snapshot", () => {
         // Given & When
-        const { container } = render(<AddTodo />)
-
+        const {container} = render(<AddTodo/>)
+        
         // Then
         expect(container).toMatchSnapshot()
     })
@@ -29,13 +31,27 @@ describe("AddTodo", () =>
         const { container, getByText } = render(<AddTodo />)
         const input = container.getElementsByTagName('input')[0]
         const button = getByText('+')
-
+    
         // When
         fireEvent.change(input, {target: {value: todoTitleMock}})
         fireEvent.click(button)
-
+    
         // Then
         expect(input.value).toBe(todoTitleMock)
         expect(mockDispatch).toBeCalledWith(addTodoRequestAction(todoTitleMock))
+    })
+    
+    it("should call setTitle when input recieve a change event", () => {
+        // Given
+        jest.spyOn(React, 'useState').mockImplementation(useStateMock);
+        const {container} = render(<AddTodo/>)
+        const input = container.getElementsByTagName('input')[0]
+        
+        // When
+        fireEvent.change(input, {target: {value: todoTitleMock}})
+        
+        // Then
+        expect(setState).toHaveBeenCalledTimes(1)
+        expect(setState).toBeCalledWith(todoTitleMock)
     })
 })
