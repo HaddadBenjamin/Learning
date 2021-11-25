1fda6e91ba6e78f7c89a3593de7c56d5ae0635de
Benjamin Haddad <haddad.b.pro@gmail.com>
2021-11-25 06:31:50 +0100

    range utility function

---
+++ Learning.TS/Array Extensions/arrayHelper.ts
@@ -0,0 +1,3 @@
+export const range = (count: number, maximum?: number) => maximum ?
+	new Array(maximum - count + 1).fill(0).map((element, index) => index + maximum - count) :
+	new Array(count).fill(0).map((element, index) => index + 1)
