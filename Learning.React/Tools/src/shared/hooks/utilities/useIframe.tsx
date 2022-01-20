ceadc45471dcfdb6d3cc6a35d82177f2fedcfe07
Benjamin Haddad <haddad.b.pro@gmail.com>
2022-01-20 17:18:26 +0100

    useIframe

---
+++ Learning.React/Tools/src/shared/hooks/utilities/useIframe.tsx
@@ -0,0 +1,45 @@
+import React, {
+  useCallback, useEffect, useRef, useState,
+} from 'react';
+import * as styles from './useIframe.module.scss';
+
+const useIframe = (url : string, title : string) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const ref = useRef<HTMLIFrameElement>(null);
+  const iframe = (
+    <div className={styles.iframeContainer}>
+      <iframe
+        className={styles.iframe}
+        ref={ref}
+        src={url}
+        title={title}
+        frameBorder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        allowFullScreen
+      />
+    </div>
+  );
+
+  const onIframeLoad = useCallback(() => {
+    const error = ref?.current?.contentDocument?.body?.getElementsByTagName('h1');
+
+    if (error) {
+      if (ref?.current?.style) ref.current.style.display = 'none';
+      setError(true);
+    }
+
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    ref.current?.addEventListener('load', onIframeLoad);
+    return () => { // eslint-disable-next-line
+      ref.current?.removeEventListener('load', onIframeLoad);
+    };
+  }, [onIframeLoad]);
+
+  return { iframe, error, isLoading };
+};
+
+export default useIframe;
