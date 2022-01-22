9c910221b98dca2273d522b04c46adc0a659e945
Benjamin Haddad <haddad.b.pro@gmail.com>
2022-01-22 14:15:36 +0100

    wip on config add typescript

--- Learning.React/Pokes/storybook/rollup.config.js
+++ Learning.React/Pokes/storybook/rollup.config.js
@@ -4,6 +4,7 @@ import external from 'rollup-plugin-peer-deps-external'
 import { terser } from 'rollup-plugin-terser'
 import postcss from 'rollup-plugin-postcss'
 import commonjs from '@rollup/plugin-commonjs';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import packageJson from './package.json';
 
 export default [
@@ -14,11 +15,13 @@ export default [
 		  { file: packageJson.module, format: 'esm', sourcemap: true }
 	  ],
 	  plugins: [
-			postcss({ plugins : [], minimize : true }),
+		  peerDepsExternal(),
+		  postcss({ modules : true,  minimize : true, plugins : [] }),
 		  babel({ exclude : 'node_modules/**', presets : ['@babel/preset-react'] }),
 		  external(),
 		  resolve({ extensions : ['.js', '.jsx', '.ts', '.tsx'] }),
 		  commonjs(),
+		  typescript(),
 		  terser()
 	  ]
   }
