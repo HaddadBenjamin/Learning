9c24bd5dae46ecfbcca430907db1fc637ada8879
Benjamin Haddad <haddad.b.pro@gmail.com>
2022-01-22 13:44:52 +0100

    wip on my package

--- Learning.React/Pokes/storybook/src/stories/Test.stories.tsx
+++ Learning.React/Pokes/storybook/src/stories/Test.stories.tsx
@@ -3,4 +3,4 @@ import { storiesOf } from '@storybook/react'
 import { Test } from '../components/Test/Test'
 
 const stories = storiesOf('Components/Test', module)
-stories.add('Test', () => <Test/>)
+stories.add('Template', () => <Test/>)
