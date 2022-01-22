fe1a7701f4349d17ce3a07a30f0c38584d4d428b
Benjamin Haddad <haddad.b.pro@gmail.com>
2022-01-22 16:40:22 +0100

    learning

--- Learning.React/Pokes/storybook/src/stories/Test.stories.tsx
+++ Learning.React/Pokes/storybook/src/stories/Test.stories.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { Test } from '../components/Test/Test'
 
 const stories = storiesOf('Components/Test', module)
-stories.add('Template', () => <Test/>)
+stories.add('Template', () => <Test a="a" b={2} />)
