464e9289dd57d5920d05b76b51f5cc87a55cf06f
Benjamin Haddad <haddad.b.pro@gmail.com>
2022-02-26 10:25:42 +0100

    fix

--- Learning.React/Tools/src/shared/hooks/utilities/useIdle.tsx
+++ Learning.React/Tools/src/shared/hooks/utilities/useIdle.tsx
@@ -1,24 +1,30 @@
 import { useEffect, useState } from 'react';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import createActivityDetector from 'activity-detector';
 
 const MILLISECONDS_IN_ONE_MINUTE = 60000;
 
-// Détecte si l'utilsiateur est inactif, ex : isIdle = useIdle(30)
+// Détecte si l'utilsiateur est inactif pendant n minutes, ex : isIdle = useIdle(30)
 const useIdle = (minutesToBeIdle : number) => {
   const [isIdle, setIsIdle] = useState(false);
 
   useEffect(() => {
-    const activityDetector = createActivityDetector({ timeToIdle: minutesToBeIdle * MILLISECONDS_IN_ONE_MINUTE });
+    let activityDetector : any;
 
-    activityDetector.on('idle', () => setIsIdle(true));
-    activityDetector.on('active', () => setIsIdle(false));
+    if (document !== undefined) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      import('activity-detector').then(({ default: createActivityDetector }) => {
+        activityDetector = createActivityDetector({ timeToIdle: minutesToBeIdle * MILLISECONDS_IN_ONE_MINUTE });
 
-    return () => activityDetector.stop();
+        activityDetector.on('idle', () => setIsIdle(true));
+        activityDetector.on('active', () => setIsIdle(false));
+      });
+    }
+
+    return () => activityDetector?.stop();
   });
 
   return isIdle;
 };
 
 export default useIdle;
+
