8ddb163f3668a5481acbcecdc8eb5d1835fa422a
Benjamin Haddad <haddad.b.pro@gmail.com>
2022-03-16 11:59:16 +0100

    useIsHover

---
+++ Learning.React/Tools/src/shared/hooks/styles/useIsHover.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef, useState } from 'react';
+
+const useIsHover = <T extends HTMLElement, >() => {
+  const [isHover, setIsHover] = useState(false);
+  const hoveredReference = useRef<T>(null);
+
+  const handleMouseOver = () => setIsHover(true);
+  const handleMouseOut = () => setIsHover(false);
+
+  useEffect(
+    // eslint-disable-next-line consistent-return
+    () => {
+      const node = hoveredReference.current;
+
+      if (node) {
+        node.addEventListener('mouseover', handleMouseOver);
+        node.addEventListener('mouseout', handleMouseOut);
+
+        return () => {
+          node.removeEventListener('mouseover', handleMouseOver);
+          node.removeEventListener('mouseout', handleMouseOut);
+        };
+      }
+    },
+    [hoveredReference.current], // Recall only if ref changes
+  );
+
+  return { hoveredReference, isHover };
+};
+
+export default useIsHover;
