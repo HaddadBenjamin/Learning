a938ceeaf0208af054068adad9a215fe24e7f935
Benjamin Haddad <haddad.b.pro@gmail.com>
2022-05-14 09:04:03 +0200

    HorizontalSwipe

---
+++ Learning.React/Pokes/horizontal-swipe/src/App.tsx
@@ -0,0 +1,8 @@
+import React, {FC} from 'react';
+import HorizontalSwipe from "./components/HorizontalSwipe/HorizontalSwipe";
+
+const App : FC = () => <div>
+  <HorizontalSwipe/>
+</div>
+
+export default App;
---
+++ Learning.React/Pokes/horizontal-swipe/src/components/Card/Card.tsx
@@ -0,0 +1,9 @@
+import React, {FC} from "react";
+// @ts-ignore
+import styles from './Card.module.scss';
+
+const Card : FC = () =>
+  <div className={styles.container}>
+  </div>
+
+export default Card;
