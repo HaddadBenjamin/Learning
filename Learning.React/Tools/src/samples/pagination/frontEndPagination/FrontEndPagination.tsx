64ede6cdf2616db419bd15e84a0390085e02626e
Benjamin Haddad <haddad.b.pro@gmail.com>
2022-05-21 15:29:11 +0200

    simpler pagination system

--- Learning.React/Tools/src/samples/pagination/frontEndPagination/FrontEndPagination.tsx
+++ Learning.React/Tools/src/samples/pagination/frontEndPagination/FrontEndPagination.tsx
@@ -2,14 +2,13 @@ import React, { FC, useState } from 'react';
 import Pagination from '../../../shared/components/Pagination/Pagination';
 
 const FrontEndPaginationSample : FC = () => {
-  const items = new Array(100).fill(0).map((e, i) => i + 1);
-  const [paginatedItems, setPaginatedItems] = useState(items);
+  const [paginatedItems, setPaginatedItems] = useState<number[]>([]);
 
   return (
     <>
       <h2>New pagination front end</h2>
       <Pagination
-        items={items}
+        items={new Array(100).fill(0).map((e, i) => i + 1)}
         pageSize={5}
         onPaginatedItemsChange={(newPaginatedItems) => setPaginatedItems(newPaginatedItems)}
       />
