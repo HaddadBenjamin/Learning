ca02544bd5481855211eae2f6a78040bfd444f1a
Benjamin Haddad <haddad.b.pro@gmail.com>
2022-05-22 12:19:43 +0200

    refactoring clickable div

--- Learning.React/Tools/src/shared/components/ClickableDiv/ClickableDiv.tsx
+++ Learning.React/Tools/src/shared/components/ClickableDiv/ClickableDiv.tsx
@@ -1,10 +1,20 @@
-import React, { FC } from 'react';
+import React, { FC, KeyboardEventHandler, MouseEventHandler } from 'react';
 
 interface Props {
-  onClick: () => void,
+  onClick: MouseEventHandler<HTMLDivElement> | undefined,
   className?: string,
 }
 
-const ClickableDiv : FC<Props> = ({ onClick, className, children }) => <div onClick={onClick} onKeyPress={onClick} role='button' tabIndex={0} className={className}>{children}</div>;
+const ClickableDiv : FC<Props> = ({ onClick, className, children }) => (
+  <div
+    onClick={onClick}
+    onKeyPress={onClick as KeyboardEventHandler<HTMLDivElement> | undefined}
+    role='button'
+    tabIndex={0}
+    className={className}
+  >
+    {children}
+  </div>
+);
 
 export default ClickableDiv;
