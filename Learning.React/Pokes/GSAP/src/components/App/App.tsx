f1619481f1b8a8b15a93aae6f4086b1211814574
Benjamin Haddad <haddad.b.pro@gmail.com>
2022-08-06 12:27:30 +0200

    text

--- Learning.React/Pokes/GSAP/src/components/App/App.tsx
+++ Learning.React/Pokes/GSAP/src/components/App/App.tsx
@@ -7,11 +7,11 @@ const App = () => <>
   <div className={styles.red}>Bloc présent pour montrer que les bloc suivants s'animent correctement</div>
 
   <FadeInOnVisibleFirstTime vars={{ x: 400 }}>
-    <div className={styles.onVisibleFirstTime}>Animation déclenché une fois lorsque l'élément est visible</div>
+    <div className={styles.onVisibleFirstTime}>Animation qui se déclenche la première fois qu'un élément est visible</div>
   </FadeInOnVisibleFirstTime>
 
   <FadeInOnVisible fromVars={{ x: -400 }} toVars={{ x: 0 }}>
-    <div className={styles.onVisible}>Animation déclenché à chaque fois que l'élément est visible</div>
+    <div className={styles.onVisible}>Animation qui se déclenche à chaque fois qu'un élément est visible</div>
   </FadeInOnVisible>
 </>
 
