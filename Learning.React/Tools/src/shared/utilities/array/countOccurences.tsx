2d0dffc78b6b5303547c5f9dbd2db5f83979b826
Benjamin Haddad <haddad.b.pro@gmail.com>
2022-09-23 12:10:00 +0200

    wip

--- Learning.React/Tools/src/shared/utilities/array/countOccurences.tsx
+++ Learning.React/Tools/src/shared/utilities/array/countOccurences.tsx
@@ -3,8 +3,7 @@ import mapToArray, { IKeyValue } from '../map/mapToArray';
 const countOccurences = <T, >(array: T[]) : IKeyValue<T, number>[] => {
   const occurencesMap = new Map<T, number>();
 
-  // eslint-disable-next-line @typescript-eslint/restrict-plus-operands,no-restricted-syntax
-  for (const element of array) occurencesMap.set(element, (occurencesMap.get(element) || 0) + 1);
+  array.forEach((element) => occurencesMap.set(element, (occurencesMap.get(element) || 0) + 1));
 
   return mapToArray(occurencesMap);
 };
