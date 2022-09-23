8778229a11acd2da81b975e6b8031948a1106be3
Benjamin Haddad <haddad.b.pro@gmail.com>
2022-09-23 12:19:48 +0200

    wip

---
+++ Learning.React/Tools/src/shared/components/lazyLoad/LazyComponent/LazyComponent.tsx
@@ -0,0 +1,24 @@
+import React, {
+  FC, MutableRefObject, useRef, Suspense,
+} from 'react';
+import useOnVisibleOnce from "../../../hooks/styles/useOnVisibleOnce";
+
+interface Props {
+  className?: string
+}
+
+// Rend les composants enfants lorsque ce wrapper est visible
+const LazyComponent : FC<Props> = ({ children, className }) => {
+  const ref = useRef() as MutableRefObject<HTMLDivElement>;
+  const isVisible = useOnVisibleOnce({ ref });
+
+  return (
+    <div ref={ref} className={className}>
+      <Suspense fallback={<div>Chargement...</div>}>
+        { isVisible && children }
+      </Suspense>
+    </div>
+  );
+};
+
+export default LazyComponent;
