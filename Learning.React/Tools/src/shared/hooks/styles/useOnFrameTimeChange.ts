dc4246a6a71c98cd26248205ea8493e8a47de0fc
Benjamin Haddad <haddad.b.pro@gmail.com>
2023-03-13 20:52:35 +0100

    useFrameTime / useDeltaTime

---
+++ Learning.React/Tools/src/shared/hooks/styles/useOnFrameTimeChange.ts
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from 'react';
+
+// Ex : useOnFrameTimeChange(setFrameTime);
+const useOnFrameTimeChange = (callback: (deltaTime: number) => void) => {
+  const requestRef = useRef<number>();
+  const previousTimeRef = useRef<number>();
+
+  const animate = (deltaTime: number) => {
+    if (previousTimeRef.current) callback(deltaTime - previousTimeRef.current);
+    previousTimeRef.current = deltaTime;
+    requestRef.current = requestAnimationFrame(animate);
+  };
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(requestRef.current!);
+  }, []);
+};
+
+export default useOnFrameTimeChange;
