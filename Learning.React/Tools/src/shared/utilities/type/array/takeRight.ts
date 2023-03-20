ecfe43c64c12a251596f341c0c8ad10547e4d214
Benjamin Haddad <haddad.b.pro@gmail.com>
2023-03-20 18:51:45 +0100

    takeRight optim

--- Learning.React/Tools/src/shared/utilities/type/array/takeRight.ts
+++ Learning.React/Tools/src/shared/utilities/type/array/takeRight.ts
@@ -1,6 +1,9 @@
-// Ex : [1,2,3] => [3], [1,2,3,4] => [3,4]
-//      ([1,2,3], -1) => [1,2,3], [1,2,3], 0) => [2,3], [1,2,3], 1) => [3]
-const takeRight = <T, >(array: T[], index = -2) : T[] => (index === -2
-  ? array.filter((_, i) => i + 1 > array.length / 2 - (array.length % 2 === 1 ? -1 : 0))
-  : array.filter((_, i) => i > index));
+// Ex :[1,2,3] => [3],
+//     [1,2,3,4] => [3,4]
+//     [1,2,3], 2 => []
+//     [1,2,3], 1 => [3]
+//     [1,2,3], 0 => [2,3]
+//     [1,2,3], -1 => [1,2,3]
+const takeRight = <T, >(array: T[], index = -2) : T[] => array.slice(index === -2 ? array.length / 2 + (array.length % 2 === 1 ? 0.5 : 0) : index + 1, array.length);
+
 export default takeRight;
