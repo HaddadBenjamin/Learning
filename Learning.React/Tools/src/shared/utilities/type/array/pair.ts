ebebe57143f0fb174261759321cc2033f96c72a9
Benjamin Haddad <haddad.b.pro@gmail.com>
2023-03-22 18:37:20 +0100

    pair

---
+++ Learning.React/Tools/src/shared/utilities/type/array/pair.ts
@@ -0,0 +1,11 @@
+// [5,1,6]        => [[5, 5], [5, 1], [5, 6], [1, 5], [1, 1], [1, 6], [6, 5], [6, 1], [6, 6]]
+// [5,1,6], false => [[5, 5], [5, 1], [5, 6], [1, 1], [1, 6], [6, 6]]
+import distinctBy from './distinctBy';
+
+const pair = <T>(array: T[], inclusive = true) : T[][] => {
+  const pairs = array.flatMap((n1) => array.map((n2) => [n1, n2]));
+
+  return inclusive ? pairs : distinctBy(pairs, (a, b) => (a[0] === b[1] && a[1] === b[0]));
+};
+
+export default pair;
