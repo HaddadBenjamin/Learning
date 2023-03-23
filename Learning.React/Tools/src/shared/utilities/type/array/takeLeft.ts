310334ac79f828bb48eb4b7c234baa00839c2f3d
Benjamin Haddad <haddad.b.pro@gmail.com>
2023-03-23 15:05:06 +0100

    fix takeLeft

--- Learning.React/Tools/src/shared/utilities/type/array/takeLeft.ts
+++ Learning.React/Tools/src/shared/utilities/type/array/takeLeft.ts
@@ -3,6 +3,6 @@
 //      [1,2,3], 0 => []
 //      [1,2,3], 1 => [1]
 //      [1,2,3], 3 => [1,2, 3]
-const takeLeft = <T, >(array: T[], index = 1) : T[] => array.slice(0, index === 0 ? 0 : array.length / 2 + (index > 0 ? index - array.length / 2 : 0));
+const takeLeft = <T, >(array: T[], index = -1) : T[] => array.slice(0, index === 0 ? 0 : array.length / 2 + (index > 0 ? index - array.length / 2 : 0));
 
 export default takeLeft;
