0fe144d5189424c284fed9abb7efbc663219c137
Benjamin Haddad <haddad.b.pro@gmail.com>
2023-03-28 16:28:47 +0200

    fix transpose matrix

--- Learning.React/Tools/src/shared/utilities/type/matrix/transpose.tsx
+++
@@ -1,7 +0,0 @@
-// Ex :
-//      [1, 2, 3]        [1, 4, 7]
-//      [4, 5, 6]   =>   [2, 5, 8]
-//      [7, 8, 9]        [3
-const transpose = <T, >(matrix: T[][]) : T[][] => matrix.map((row, i) => row.map((_, j) => matrix[j][i]));
-
-export default transpose;
