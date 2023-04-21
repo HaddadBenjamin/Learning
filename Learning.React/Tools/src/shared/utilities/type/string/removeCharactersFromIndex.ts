e0b853be27b7795a1b7ed92a79c083c6acf7cdbe
Benjamin Haddad <haddad.b.pro@gmail.com>
2023-04-21 10:48:52 +0200

    character utilities

--- Learning.React/Tools/src/shared/utilities/type/string/removeCharactersFromIndex.ts
+++ Learning.React/Tools/src/shared/utilities/type/string/removeCharactersFromIndex.ts
@@ -1,3 +1,3 @@
-const removeCharactersFromIndex = (text: string, index: number, characterCount = 1) => `${text.slice(0, index)}${text.slice(index + characterCount)}`;
+const removeCharactersFromIndex = (text: string, index: number, characterCount = 1) : string => `${text.slice(0, index)}${text.slice(index + characterCount)}`;
 
 export default removeCharactersFromIndex;
