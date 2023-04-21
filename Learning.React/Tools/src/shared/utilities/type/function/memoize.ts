69d1e597e83015ddbe607c755a1946ab163c057b
Benjamin Haddad <haddad.b.pro@gmail.com>
2023-04-21 19:30:19 +0200

    fix memoize

--- Learning.React/Tools/src/shared/utilities/type/function/memoize.ts
+++ Learning.React/Tools/src/shared/utilities/type/function/memoize.ts
@@ -10,9 +10,9 @@ const memoize = <T extends(...args: any[]) => any>(func: T): MemoizedFunc<T> => 
   return function (...args: Parameters<T>): ReturnType<T> {
     const key = JSON.stringify(args);
 
-    if (cache.has(key)) {
-      return cache.get(key)!;
-    }
+    if (cache.has(key)) return cache.get(key)!;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     const result = func.apply(this, args);
 
     cache.set(key, result);
